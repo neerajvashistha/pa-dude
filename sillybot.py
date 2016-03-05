b9c1e701e0724476b728db66851957e97e1f9e58
neerajvashistha <neerajvash8@gmail.com>
2016-03-05 13:25:29 +0530

    intelliDude

---
+++ sillybot.py
@@ -0,0 +1,22 @@
+import aiml
+import os
+
+kernel = aiml.Kernel()
+
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "std_startup.xml", commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+# kernel now ready for use
+while True:
+    message = raw_input("Enter your message to the bot: ")
+    if message == "quit":
+        exit()
+    elif message == "save":
+        kernel.saveBrain("bot_brain.brn")
+    else:
+        bot_response = kernel.respond(message)
+        # Do something with bot_response
+        print bot_response
